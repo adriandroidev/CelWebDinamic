8f851f8ba984d18e1b9773acb9a2cbbcd121bc63
Adrian <adrianzepeda020200@gmail.com>
2023-04-11 11:38:00 -0600

    AdministracionUser

---
+++ DAL/DAL_Formularios.cs
@@ -0,0 +1,69 @@
+﻿using EL;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DAL
+{
+    public class DAL_Formularios
+    {
+        public static Formularios Insert(Formularios Entidad)
+        {
+            using (BDCelWeb bd = new BDCelWeb())
+            {
+                Entidad.Activo = true;
+                Entidad.FechaRegistro = DateTime.Now;
+                bd.Formularios.Add(Entidad);
+                bd.SaveChanges();
+                return Entidad;
+            }
+        }
+        public static bool Update(Formularios Entidad)
+        {
+            using (BDCelWeb bd = new BDCelWeb())
+            {
+                var RegistroBD = bd.Formularios.Find(Entidad.IdFormulario);
+                RegistroBD.Formulario = Entidad.Formulario;
+                RegistroBD.IdUsuarioActualizado = Entidad.IdUsuarioActualizado;
+                RegistroBD.FechaActualizado = DateTime.Now;
+                return bd.SaveChanges() > 0;
+            }
+        }
+        public static bool Delete(Formularios Entidad)
+        {
+            using (BDCelWeb bd = new BDCelWeb())
+            {
+                var RegistroBD = bd.Formularios.Find(Entidad.IdFormulario);
+                RegistroBD.Activo = false;
+                RegistroBD.IdUsuarioActualizado = Entidad.IdUsuarioActualizado;
+                RegistroBD.FechaActualizado = DateTime.Now;
+                return bd.SaveChanges() > 0;
+            }
+        }
+        public static List<Formularios> List(bool Activo = true)
+        {
+            using (BDCelWeb bd = new BDCelWeb())
+            {
+                return bd.Formularios.Where(a => a.Activo == Activo).ToList();
+            }
+        }
+        public static Formularios Registro(int IdRegistro)
+        {
+            using (BDCelWeb bd = new BDCelWeb())
+            {
+                return bd.Formularios.Find(IdRegistro);
+            }
+        }
+        public static List<Formularios> Buscar(string Palabra)
+        {
+            using (BDCelWeb bd = new BDCelWeb())
+            {
+                var Consulta = bd.Formularios.Where(a => a.Formulario.Contains(Palabra)).ToList();
+                return Consulta;
+            }
+        }
+
+    }
+}
---
+++ DAL/DAL_RolFormularios.cs
@@ -0,0 +1,62 @@
+﻿using EL;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DAL
+{
+    public class DAL_RolFormulario
+    {
+        public static RolFormularios Insert(RolFormularios Entidad)
+        {
+            using (BDCelWeb bd = new BDCelWeb())
+            {
+                Entidad.Activo = true;
+                Entidad.FechaRegistro = DateTime.Now;
+                bd.RolFormularios.Add(Entidad);
+                bd.SaveChanges();
+                return Entidad;
+            }
+        }
+        public static bool Update(RolFormularios Entidad)
+        {
+            using (BDCelWeb bd = new BDCelWeb())
+            {
+                var RegistroBD = bd.RolFormularios.Find(Entidad.IdRolFormulario);
+                RegistroBD.IdRol = Entidad.IdRol;
+                RegistroBD.IdFormulario = Entidad.IdFormulario;
+                RegistroBD.IdUsuarioActualizado = Entidad.IdUsuarioActualizado;
+                RegistroBD.FechaActualizado = DateTime.Now;
+                return bd.SaveChanges() > 0;
+            }
+        }
+        public static bool Delete(RolFormularios Entidad)
+        {
+            using (BDCelWeb bd = new BDCelWeb())
+            {
+                var RegistroBD = bd.RolFormularios.Find(Entidad.IdRolFormulario);
+                RegistroBD.Activo = false;
+                RegistroBD.IdUsuarioActualizado = Entidad.IdUsuarioActualizado;
+                RegistroBD.FechaActualizado = DateTime.Now;
+                return bd.SaveChanges() > 0;
+            }
+        }
+        public static List<RolFormularios> List(int IdRol, bool Activo = true)
+        {
+            using (BDCelWeb bd = new BDCelWeb())
+            {
+                return bd.RolFormularios.Where(a => a.IdRol == IdRol && a.Activo == Activo).ToList();
+            }
+        }
+        public static RolFormularios Registro(int IdRegistro)
+        {
+            using (BDCelWeb bd = new BDCelWeb())
+            {
+                return bd.RolFormularios.Find(IdRegistro);
+            }
+        }
+
+    }
+}
